5c09bcc83af957f14aa9367c4f3cb055ccd47c41
DominicSherman <dominic.sherman98@gmail.com>
2018-08-30 15:13:53 -0500

    Add gitignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import image from './image.jpg';
+import image from './assets/image.jpg';
+import flowers from './assets/flowers.png';
 import './App.css';
 import {Column, Row} from 'simple-flexbox';
 
@@ -7,20 +8,26 @@ export default class App extends Component {
     render() {
 
         return (
-            <div className="App">
-                <Row horizontal={'space-between'}>
-                    <p>Home</p>
-                    <p>Pictures</p>
-                    <p>The Bridal Party</p>
-                    <p>Her Story</p>
-                    <p>His Story</p>
-                    <p>The Best Man's Story</p>
-                </Row>
-                <Column className="App-header" vertical={'center'}>
-                    <h1 className="App-title">Dominic + Mary</h1>
+            <div className={"App"}>
+                <header className={"App-nav"}>
+                    <div className={"App-white"}/>
+                    <Row horizontal={'space-between'} className={"App-navColor"}>
+                        <p>Home</p>
+                        <p>Pictures</p>
+                        <p>The Bridal Party</p>
+                        <p>Her Story</p>
+                        <p>His Story</p>
+                        <p>The Best Man's Story</p>
+                    </Row>
+                    <img src={flowers} alt=''/>
+                </header>
+                <Column className={"App-header"} vertical={'center'}>
+                    <header className={"App-title"}>Dominic</header>
+                    <header className={"App-title"}>+</header>
+                    <header className={"App-title"}>Mary</header>
                 </Column>
-                <p className="App-intro">
-                    <img src={image}/>
+                <p className={"App-image"}>
+                    <img src={image} alt=''/>
                 </p>
             </div>
         );
