0f4216ff07bb16efac1a4a5a51cef01c2ae29141
DominicSherman <dominic.sherman98@gmail.com>
2018-08-30 11:11:06 -0500

    Starting to format

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import image from './image.jpg';
 import './App.css';
+import {Column, Row} from 'simple-flexbox';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default class App extends Component {
+    render() {
+
+        return (
+            <div className="App">
+                <Row horizontal={'space-between'}>
+                    <p>Home</p>
+                    <p>Pictures</p>
+                    <p>The Bridal Party</p>
+                    <p>Her Story</p>
+                    <p>His Story</p>
+                    <p>The Best Man's Story</p>
+                </Row>
+                <Column className="App-header" vertical={'center'}>
+                    <h1 className="App-title">Dominic + Mary</h1>
+                </Column>
+                <p className="App-intro">
+                    <img src={image}/>
+                </p>
+            </div>
+        );
+    }
 }
-
-export default App;
