4ad2a1e3ef9f405ab17c866fb3db308db9d6c201
DominicSherman <dominic.sherman98@gmail.com>
2018-09-02 17:28:17 -0500

    Add loading

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import {Row} from "simple-flexbox";
+import LoadingScreen from 'react-loading-screen';
 
 import headerImage from './assets/images/header.png';
 import Home from "./screens/Home";
@@ -14,7 +15,8 @@ import HeaderLink from "./components/HeaderLink";
 
 export default class Main extends Component {
     state = {
-        isSticky: false
+        isSticky: false,
+        isLoading: true
     };
 
     componentDidMount() {
@@ -24,69 +26,76 @@ export default class Main extends Component {
 
     render() {
         return (
-            <Router>
-                <div>
-                    <img
-                        alt=''
-                        className={'Main-headerImage'}
-                        id={'headerImage'}
-                        src={headerImage}
-                    />
-                    <div className={'Main-navBarContainer'} style={{
-                        position: this.state.isSticky ? 'fixed' : 'relative',
-                        top: 0
-                    }}>
-                        <Row
-                            className={'Main-navBar'}
-                            horizontal={'space-between'}
-                        >
-                            <HeaderLink
-                                route={'/'}
-                                text={'Home'}
-                            />
-                            <HeaderLink
-                                route={'/pictures'}
-                                text={'Pictures'}
-                            />
-                            <HeaderLink
-                                route={'/bridal-party'}
-                                text={'Bridal Party'}
-                            />
-                            <HeaderLink
-                                route={'/her-story'}
-                                text={'Her Story'}
-                            />
-                            <HeaderLink
-                                route={'/his-story'}
-                                text={'His Story'}
-                            />
-                            <HeaderLink
-                                route={'/best-man-story'}
-                                text={'Best Man\'s Story'}
-                            />
-                        </Row>
-                    </div>
+            <LoadingScreen
+                loading={this.state.isLoading}
+                bgColor={'lightgrey'}
+                spinnerColor={'lightblue'}
+            >
+                <Router>
+                    <div>
+                        <img
+                            alt=''
+                            className={'Main-headerImage'}
+                            id={'headerImage'}
+                            onLoad={() => this.setState({isLoading: false})}
+                            src={headerImage}
+                        />
+                        <div className={'Main-navBarContainer'} style={{
+                            position: this.state.isSticky ? 'fixed' : 'relative',
+                            top: 0
+                        }}>
+                            <Row
+                                className={'Main-navBar'}
+                                horizontal={'space-between'}
+                            >
+                                <HeaderLink
+                                    route={'/'}
+                                    text={'Home'}
+                                />
+                                <HeaderLink
+                                    route={'/pictures'}
+                                    text={'Pictures'}
+                                />
+                                <HeaderLink
+                                    route={'/bridal-party'}
+                                    text={'Bridal Party'}
+                                />
+                                <HeaderLink
+                                    route={'/her-story'}
+                                    text={'Her Story'}
+                                />
+                                <HeaderLink
+                                    route={'/his-story'}
+                                    text={'His Story'}
+                                />
+                                <HeaderLink
+                                    route={'/best-man-story'}
+                                    text={'Best Man\'s Story'}
+                                />
+                            </Row>
+                        </div>
 
-                    <Route exact path="/" component={
-                        () => <Home isSticky={this.state.isSticky}/>
-                    }/>
-                    <Route exact path="/pictures" component={
-                        () => <Pictures isSticky={this.state.isSticky}/>
-                    }/>
-                    <Route exact path="/bridal-party" component={
-                        () => <BridalParty isSticky={this.state.isSticky}/>
-                    }/>
-                    <Route exact path="/her-story" component={
-                        () => <HerStory isSticky={this.state.isSticky}/>
-                    }/>
-                    <Route exact path="/his-story" component={
-                        () => <HisStory isSticky={this.state.isSticky}/>
-                    }/>
-                    <Route exact path="/best-man-story" component={
-                        () => <BestManStory isSticky={this.state.isSticky}/>
-                    }/>
-                </div>
-            </Router>
+                        <Route exact path="/" component={
+                            () => <Home isSticky={this.state.isSticky}/>
+                        }/>
+                        <Route exact path="/pictures" component={
+                            () => <Pictures isSticky={this.state.isSticky}/>
+                        }/>
+                        <Route exact path="/bridal-party" component={
+                            () => <BridalParty isSticky={this.state.isSticky}/>
+                        }/>
+                        <Route exact path="/her-story" component={
+                            () => <HerStory isSticky={this.state.isSticky}/>
+                        }/>
+                        <Route exact path="/his-story" component={
+                            () => <HisStory isSticky={this.state.isSticky}/>
+                        }/>
+                        <Route exact path="/best-man-story" component={
+                            () => <BestManStory isSticky={this.state.isSticky}/>
+                        }/>
+                    </div>
+                </Router>
+            </LoadingScreen>
         );
     }
 }
