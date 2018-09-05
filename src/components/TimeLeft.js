fab9e50225c56d0e1794ec0c6e27ac1e108cb139
DominicSherman <dominic.sherman98@gmail.com>
2018-09-04 21:06:21 -0500

    Remove hours minutes and seconds from time left

--- src/components/TimeLeft.js
+++ src/components/TimeLeft.js
@@ -5,53 +5,24 @@ export default class TimeLeft extends Component {
         super(props);
 
         this.state = {
-            days: 0,
-            hours: 0,
-            minutes: 0,
-            seconds: 0
+            days: 0
         };
     }
 
-    tick() {
+    componentDidMount() {
         const oneDay = 24 * 60 * 60 * 1000;
-        const oneHour = 60 * 60 * 1000;
-        const oneMinute = 60 * 1000;
-        const oneSecond = 1000;
         const weddingDay = new Date("2019-05-28T14:00:00-07:00").getTime();
         const now = Date.now();
         const days = Math.floor(Math.abs((weddingDay - now) / (oneDay)));
-        const hoursRemainder = Math.abs((weddingDay - now)) % (oneDay);
-        const hours = Math.floor(Math.abs(hoursRemainder / oneHour));
-        const minutesRemainder = Math.abs(hoursRemainder % oneHour);
-        const minutes = Math.floor(Math.abs(minutesRemainder / oneMinute));
-        const secondsRemainder = Math.abs(minutesRemainder % oneMinute);
-        const seconds = Math.floor(secondsRemainder / oneSecond);
 
         this.setState({
-            days,
-            hours,
-            minutes,
-            seconds
+            days
         });
-    }
-
-    componentDidMount() {
-        this.intervalID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.intervalID);
     }
 
     render() {
         return (
-            this.state.days ?
-                <p>{`${this.state.days} days, ${this.state.hours} hours, ${this.state.minutes} minutes, ${this.state.seconds} seconds`}</p>
-                :
-                <p>{'Calculating...'}</p>
+            <p>{`${this.state.days} days`}</p>
         );
     }
 }
