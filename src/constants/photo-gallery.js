507aee1692be1b714807d8180d590fd3463d78bf
DominicSherman <dominic.sherman98@gmail.com>
2018-09-11 13:40:43 -0500

    Refactor image names

--- src/constants/photo-gallery.js
+++ src/constants/photo-gallery.js
@@ -1,81 +1,81 @@
-import one from '../assets/gallery/1.jpg';
-import two from '../assets/gallery/2.jpg';
-import three from '../assets/gallery/3.jpg';
-import four from '../assets/gallery/4.jpg';
-import five from '../assets/gallery/5.jpg';
-import six from '../assets/gallery/6.jpg';
-import seven from '../assets/gallery/7.jpg';
-import eight from '../assets/gallery/8.jpg';
-import nine from '../assets/gallery/9.jpg';
-import ten from '../assets/gallery/10.jpg';
-import eleven from '../assets/gallery/11.jpg';
-import twelve from '../assets/gallery/12.jpg';
-import thirteen from '../assets/gallery/13.jpg';
+import April2018 from '../assets/gallery/April2018.jpg';
+import July2017 from '../assets/gallery/July2017.jpg';
+import January2018 from '../assets/gallery/January2018.jpg';
+import June2018 from '../assets/gallery/June2018.jpg';
+import August2018 from '../assets/gallery/August2018.jpg';
+import May2018 from '../assets/gallery/May2018.jpg';
+import November2017 from '../assets/gallery/November2017.jpg';
+import March2018 from '../assets/gallery/March2018.jpg';
+import July20172 from '../assets/gallery/July2017-2.jpg';
+import August2017 from '../assets/gallery/August2017.jpg';
+import June20182 from '../assets/gallery/June2018-2.jpg';
+import October2017 from '../assets/gallery/October2017.jpg';
+import August20182 from '../assets/gallery/August2018-2.jpg';
 
 export const photos = [
     {
-        original: one,
-        thumbnail: one,
-        description: 'April 2018'
-    },
-    {
-        original: two,
-        thumbnail: two,
+        original: July2017,
+        thumbnail: July2017,
         description: 'July 2017'
     },
     {
-        original: three,
-        thumbnail: three,
-        description: 'January 2018'
-    },
-    {
-        original: four,
-        thumbnail: four,
-        description: 'June 2018'
-    },
-    {
-        original: five,
-        thumbnail: five,
-        description: 'August 2018'
-    },
-    {
-        original: six,
-        thumbnail: six,
-        description: 'May 2018'
-    },
-    {
-        original: seven,
-        thumbnail: seven,
-        description: 'November 2017'
-    },
-    {
-        original: eight,
-        thumbnail: eight,
-        description: 'March 2018'
-    },
-    {
-        original: nine,
-        thumbnail: nine,
+        original: July20172,
+        thumbnail: July20172,
         description: 'July 2017'
     },
     {
-        original: ten,
-        thumbnail: ten,
+        original: August2017,
+        thumbnail: August2017,
         description: 'August 2017'
     },
     {
-        original: eleven,
-        thumbnail: eleven,
-        description: 'June 2018'
-    },
-    {
-        original: twelve,
-        thumbnail: twelve,
+        original: October2017,
+        thumbnail: October2017,
         description: 'October 2017'
     },
     {
-        original: thirteen,
-        thumbnail: thirteen,
+        original: November2017,
+        thumbnail: November2017,
+        description: 'November 2017'
+    },
+    {
+        original: January2018,
+        thumbnail: January2018,
+        description: 'January 2018'
+    },
+    {
+        original: March2018,
+        thumbnail: March2018,
+        description: 'March 2018'
+    },
+    {
+        original: April2018,
+        thumbnail: April2018,
+        description: 'April 2018'
+    },
+    {
+        original: May2018,
+        thumbnail: May2018,
+        description: 'May 2018'
+    },
+    {
+        original: June2018,
+        thumbnail: June2018,
+        description: 'June 2018'
+    },
+    {
+        original: June20182,
+        thumbnail: June20182,
+        description: 'June 2018'
+    },
+    {
+        original: August2018,
+        thumbnail: August2018,
+        description: 'August 2018'
+    },
+    {
+        original: August20182,
+        thumbnail: August20182,
         description: 'August 2018'
     }
 
