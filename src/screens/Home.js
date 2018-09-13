d3c7cc1f5ee9de406d7115a730d0b68d6cbfa83a
DominicSherman <dominic.sherman98@gmail.com>
2018-09-13 12:04:30 -0500

    Cleaning it up more

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -55,7 +55,7 @@ export default class Home extends Component {
                     </div>
                 </div>
 
-                < div
+                <div
                     className={'Home-information column center'}>
                     < div
                         className={'row spaceBetween'}>
