aca386bc813dc0500fd7309907eee104972af01f
DominicSherman <dominic.sherman98@gmail.com>
2018-09-13 13:00:18 -0500

    Add buttons

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,8 @@ import '../css/Home.css';
 import church from '../assets/graphics/church.png';
 import cake from '../assets/graphics/cake.png';
 import bells from '../assets/graphics/weddingsbells.png';
+import leftGraphic from '../assets/graphics/rsvpleft.png';
+import rightGraphic from '../assets/graphics/rsvpright.png';
 
 export default class Home extends Component {
     constructor(props) {
@@ -30,6 +32,7 @@ export default class Home extends Component {
         return (
             <div className={'column'}>
                 <div className={'row'}>
+
                     <div className={'Home-welcome column'}>
                         <div className={'row spaceBetween'}>
                             <div className={'column'}>
@@ -43,86 +46,83 @@ export default class Home extends Component {
                             />
                             <span className={'Home-bellBorder'}/>
                         </div>
-                        <p>{'You can find information about the wedding, get directions, RSVP, and afterwards you can view pictures from the day here!'}</p>
+                        <p>{'You can find information about the wedding, get directions, RSVP, and afterwards you can view pictures from the day here! '}</p>
                     </div>
-                    <div className={'Home-daysLeft column spaceBetween'}>
+
+                    <div className={'Home-countdown column spaceBetween'}>
                         <div className={'Home-weddingBegins'}>
                             <p className={'Home-weddingBeginsText'}>{'Wedding begins in'}</p>
                         </div>
-                        <div className={'Home-days'}>
-                            <p className={'Home-daysText'}>{`${this.state.days} DAYS`}</p>
+                        <div className={'Home-daysLeft'}>
+                            <p className={'Home-daysLeftText'}>{`${this.state.days} DAYS`}</p>
                         </div>
                     </div>
+
                 </div>
 
-                <div
-                    className={'Home-information column center'}>
-                    < div
-                        className={'row spaceBetween'}>
-                        < div
-                            className={'Home-informationTextWrapper column'}>
-                            < p
-                                className={'Home-informationHeaderText'}> {'THE CEREMONY'}
-                            </p>
-                            <
-                                p
-                                className={'Home-locationText'}> {'Holy Rosary Church'}
-                            </p>
-                            <
-                                p> {'May 28th, 2019 at 2PM'}
-                            </p>
-                            <
-                                a
-                                href={holyRosaryLink}
-                                target={'_blank'}>
-                                < p> {'375 NE Clackamas St. Portland, OR 97232'}
-                                </p>
-                                <
-                                /a>
+                <div className={'Home-information column center'}>
+
+                    <div className={'row spaceBetween'}>
+                        <div className={'Home-informationTextWrapper column'}>
+                            <p className={'Home-headerText'}> {'THE CEREMONY'}</p>
+                            <p className={'Home-locationText'}> {'Holy Rosary Church'}</p>
+                            <p> {'May 28th, 2019 at 2PM'}</p>
+                            <a href={holyRosaryLink} target={'_blank'}>
+                                <p> {'375 NE Clackamas St. Portland, OR 97232'}</p></a>
                         </div>
-                        <
-                            div
-                            className={'Home-logoWrapper row'}>
-                            < img
+                        <div className={'Home-logoWrapper row'}>
+                            <img
                                 alt={''}
                                 className={'Home-logo'}
                                 src={church}
                             />
                             <span className={'Home-topRightBorder'}/>
-                            <
-                            /div>
                         </div>
-                        <
-                            span
-                            className={'Home-leftBorder'}
-                        />
-                        <div className={'row spaceBetween'}>
-                            <div className={'Home-logoWrapper row'}>
-                                <img
-                                    alt={''}
-                                    className={'Home-logo'}
-                                    src={cake}
-                                />
-                            </div>
-                            <div
-                                className={'Home-informationTextWrapper column right'}
-                                style={{textAlign: 'right'}}
-                            >
-                                <p className={'Home-informationHeaderText'}>
-                                    {'THE RECEPTION'}
-                                </p>
-                                <p className={'Home-locationText'}>{'MCMENAMIN\'S CORNELIUS PASS'}</p>
-                                <p>{'May 28th, 2019 at 3:30PM'}</p>
-                                <a href={mcmenaminsLink} target={'_blank'}>
-                                    <p>{'4045 NE Cornelius Pass Rd. Hillsboro, OR 97124'}</p>
-                                </a>
-                                <span className={'Home-bottomRightBorder'}/>
-                            </div>
-                        </div>
-                        <
-                        /div>
                     </div>
-                    )
-                    ;
-                    }
-                    }
+
+                    <div className={'row spaceBetween'}>
+                        <div className={'Home-logoWrapper row'}>
+                            <img
+                                alt={''}
+                                className={'Home-logo'}
+                                src={cake}
+                            />
+                        </div>
+                        <div
+                            className={'Home-informationTextWrapper column right'}
+                            style={{textAlign: 'right'}}
+                        >
+                            <p className={'Home-headerText'}>
+                                {'THE RECEPTION'}
+                            </p>
+                            <p className={'Home-locationText'}>{'MCMENAMIN\'S CORNELIUS PASS'}</p>
+                            <p>{'May 28th, 2019 at 3:30PM'}</p>
+                            <a href={mcmenaminsLink} target={'_blank'}>
+                                <p>{'4045 NE Cornelius Pass Rd. Hillsboro, OR 97124'}</p>
+                            </a>
+                            <span className={'Home-bottomRightBorder'}/>
+                        </div>
+                    </div>
+
+                    <span className={'Home-leftBorder'}/>
+
+                    <div className={'Home-buttons row spaceBetween'}>
+                        <img
+                            alt={''}
+                            className={'Home-rsvpGraphic'}
+                            src={leftGraphic}
+                        />
+                        <button className={'Home-button'}>{'RSVP'}</button>
+                        <button className={'Home-button2'}>{'Download the App'}</button>
+                        <img
+                            alt={''}
+                            className={'Home-rsvpGraphic'}
+                            src={rightGraphic}
+                        />
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
