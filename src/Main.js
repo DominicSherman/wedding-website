1443b39e7dedb7485e927f78015298a1191661a2
DominicSherman <dominic.sherman98@gmail.com>
2018-09-26 16:56:41 -0500

    Fix redux thigns

--- src/Main.js
+++ src/Main.js
@@ -9,7 +9,6 @@ import Routing from './Routing';
 import {withRouter} from 'react-router-dom';
 import {initializeFirebase} from './services/firebase-service';
 import RSVPInformation from './screens/RSVPInformation';
-import {withRedux} from './redux-factory';
 
 let headerImageRef;
 
@@ -72,4 +71,4 @@ class Main extends Component {
     }
 }
 
-export default withRedux(withRouter(Main));
+export default withRouter(Main);
