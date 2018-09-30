19dcca6cfb2793a2a32e9c5912266f978f4d45ec
DominicSherman <dominic.sherman98@gmail.com>
2018-09-30 18:32:21 -0500

    Add registry information and bios

--- src/constants/bridal-party.js
+++ src/constants/bridal-party.js
@@ -16,42 +16,42 @@ export const groomsmen = [
         name: 'Gabriel Sherman',
         role: 'Best Man',
         relation: 'Brother of the Groom',
-        bio: '',
+        bio: 'Fancy college boy studying something smart. Is only the best man because the groom doesn’t have any friends.',
         image: gabe
     },
     {
         name: 'John Paul Foxhoven',
         role: 'Groomsman',
         relation: 'Uncle of the Groom',
-        bio: '',
+        bio: 'Nurse practitioner in New Jersey. Has won a lot of candy in poker.',
         image: johnpaul
     },
     {
         name: 'Michael Sherman',
         role: 'Groomsman',
         relation: 'Brother of the Groom',
-        bio: '',
+        bio: 'Graphic designer in Dallas. Big ole thick ole boy and assistant to the best man.',
         image: michael
     },
     {
         name: 'Spencer Hancock',
         role: 'Groomsman',
         relation: 'Friend of the Groom',
-        bio: '',
+        bio: 'Single and ready to mingle. Any woman would be lucky to have him. 😉',
         image: spencer
     },
     {
         name: 'John Edson',
         role: 'Groomsman',
         relation: 'Brother of the Bride',
-        bio: '',
+        bio: 'Looks like a koala, has the short term memory of a fish, and should probably be tested for narcolepsy.',
         image: janis
     },
     {
         name: 'Thomas Edson',
         role: 'Groomsman',
         relation: 'Brother of the Bride',
-        bio: '',
+        bio: 'Has the greatest capacity for both good and evil.',
         image: thomas
     }
 ];
@@ -61,42 +61,42 @@ export const bridesmaids = [
         name: 'Rose Edson',
         role: 'Maid of Honor',
         relation: 'Sister of the Bride',
-        bio: '',
+        bio: 'Medical assistant who is always on time.  Warning - hide all of your leftovers if she is near.',
         image: rose
     },
     {
         name: 'Katherine Edson',
         role: 'Bridesmaid',
         relation: 'Sister of the Bride',
-        bio: '',
+        bio: 'Professional administrative assistant. Basically planned this wedding.',
         image: katherine
     },
     {
         name: 'Regina Sherman',
         role: 'Bridesmaid',
         relation: 'Sister of the Groom',
-        bio: '',
+        bio: 'FedEx print supervisor.  Adult in training. Loves potatoes and will definitely cry at this wedding.',
         image: regina
     },
     {
         name: 'Valda Edson',
         role: 'Bridesmaid',
         relation: 'Cousin of the Bride',
-        bio: '',
+        bio: 'Medical assistant in Colorado. Is very kind to all people but there’s a definite possibility that she actually hates you.',
         image: valda
     },
     {
         name: 'Esther Edson',
         role: 'Bridesmaid',
         relation: 'Sister of the Bride',
-        bio: '',
+        bio: 'Loves all things Italian. Mommy of one. Looks normal but be careful.',
         image: esther
     },
     {
         name: 'Maria Sherman',
         role: 'Bridesmaid',
         relation: 'Sister of the Groom',
-        bio: '',
+        bio: 'The most helpful 16 year old on this earth. Has now replaced the groom as the perfect child.',
         image: maria
     }
 ];
--- src/screens/Registry.js
+++ src/screens/Registry.js
@@ -1,12 +1,43 @@
 import React, {Component} from 'react';
 
 import '../css/screens/Registry.css';
+import macy from '../assets/macy.png';
+import crate from '../assets/crateandbarrel.png';
+import {crateAndBarrelLink, macyLink} from '../constants/website-links';
 
 export default class Registry extends Component {
     render() {
         return (
-            <div className={'row center'}>
-                <p className={'ComingSoon'}>{'Coming soon!'}</p>
+            <div className={'Registry-wrapper column center'}>
+                <p>{'Click on a logo to go to the registry'}</p>
+                <div className={'row spaceBetween'}>
+                    <div className={'Registry-logoWrapper'}>
+                        <a
+                            href={macyLink}
+                            target={'_blank'}
+                        >
+                            <img
+                                alt={''}
+                                className={'Registry-logo'}
+                                src={macy}
+
+                            />
+                        </a>
+                    </div>
+                    <div className={'Registry-logoWrapper'}>
+                        <a
+                            href={crateAndBarrelLink}
+                            target={'_blank'}
+                        >
+                            <img
+                                alt={''}
+                                className={'Registry-logo'}
+                                src={crate}
+
+                            />
+                        </a>
+                    </div>
+                </div>
             </div>
         );
     }
