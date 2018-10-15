949b1b9da4000e9b26cb07b1ef04deb1990b7150
Dominic <dsherman@hy-vee.com>
2018-10-15 08:53:49 -0500

    Linting and styling

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: 'get-off-my-lawn',
+    rules: {
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'max-params': ['error', 4],
+        'react/prop-types': 'off'
+    }
+};
