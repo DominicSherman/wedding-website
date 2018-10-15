119278ad7d78c3c96acb6a50d8ccf95aa6f90d18
Dominic <dsherman@hy-vee.com>
2018-10-15 13:53:16 -0500

    Revert "Add babel-eslint"
    This reverts commit 2776673128a3cd66665caa0f127a992d227c3d17.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
-    parser: 'babel-eslint',
     extends: 'get-off-my-lawn',
     rules: {
         'jsx-a11y/click-events-have-key-events': 'off',
         'max-params': ['error', 4],
-        'react/prop-types': 'off',
-        'node/no-unpublished-import': 0
+        'react/prop-types': 'off'
     }
 };
