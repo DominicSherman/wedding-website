2776673128a3cd66665caa0f127a992d227c3d17
Dominic <dsherman@hy-vee.com>
2018-10-15 12:32:10 -0500

    Add babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
+    parser: 'babel-eslint',
     extends: 'get-off-my-lawn',
     rules: {
         'jsx-a11y/click-events-have-key-events': 'off',
         'max-params': ['error', 4],
-        'react/prop-types': 'off'
+        'react/prop-types': 'off',
+        'node/no-unpublished-import': 0
     }
 };
