d06afda56b4dab9f22d795df83094abd95e02a7f
DominicSherman <dominic.sherman98@gmail.com>
2018-10-18 19:20:44 -0500

    Testingggg

--- test/components/NavBar.test.js
+++ test/components/NavBar.test.js
@@ -1,18 +1,112 @@
 import React from 'react';
 import Chance from 'chance';
 import ShallowRenderer from 'react-test-renderer/shallow';
-import BrideGroom from '../../src/components/BrideGroom';
+import NavBar from '../../src/components/NavBar';
+import HeaderLink from '../../src/components/HeaderLink';
 
 const chance = new Chance();
 
 describe('NavBar', () => {
+    let renderedComponent,
+        renderedWrapper,
+
+        renderedHomeLink,
+        renderedOurStoryLink,
+        renderedWeddingPartyLink,
+        renderedPicturesLink,
+        renderedRegistryLink,
+
+        expectedProps;
+
+    const cacheChildren = () => {
+        renderedWrapper = renderedComponent.props.children;
+
+        [
+            renderedHomeLink,
+            renderedOurStoryLink,
+            renderedWeddingPartyLink,
+            renderedPicturesLink,
+            renderedRegistryLink
+        ] = renderedWrapper.props.children;
+    };
+
     const renderComponent = () => {
         const shallowRenderer = ShallowRenderer.createRenderer();
 
-        shallowRenderer.render(<BrideGroom {...expectedProps}/>);
+        shallowRenderer.render(<NavBar {...expectedProps}/>);
 
         renderedComponent = shallowRenderer.getRenderOutput();
 
         cacheChildren();
     };
+
+    beforeEach(() => {
+        expectedProps = {
+            isSticky: chance.bool(),
+            location: {
+                pathname: chance.string()
+            }
+        };
+
+        renderComponent();
+    });
+
+    it('should render a root div', () => {
+        expect(renderedComponent.type).toBe('div');
+        expect(renderedComponent.props.className).toBe('NavBar-wrapper center');
+    });
+
+    it('should have fixed position if isSticky', () => {
+        expectedProps.isSticky = true;
+        renderComponent();
+
+        expect(renderedComponent.props.style.position).toBe('fixed');
+    });
+
+    it('should have relative position if it is not sticky', () => {
+        expectedProps.isSticky = false;
+        renderComponent();
+
+        expect(renderedComponent.props.style.position).toBe('relative');
+    });
+
+    it('should render a wrapper div', () => {
+        expect(renderedWrapper.type).toBe('div');
+        expect(renderedWrapper.props.className).toBe('NavBar-body spaceBetween');
+    });
+
+    it('should render a HeaderLink for home', () => {
+        expect(renderedHomeLink.type).toBe(HeaderLink);
+        expect(renderedHomeLink.props.selected).toBe(expectedProps.location.pathname);
+        expect(renderedHomeLink.props.route).toBe('/');
+        expect(renderedHomeLink.props.text).toBe('HOME');
+    });
+
+    it('should render a HeaderLink for our story', () => {
+        expect(renderedOurStoryLink.type).toBe(HeaderLink);
+        expect(renderedOurStoryLink.props.selected).toBe(expectedProps.location.pathname);
+        expect(renderedOurStoryLink.props.route).toBe('/our-story');
+        expect(renderedOurStoryLink.props.text).toBe('OUR STORY');
+    });
+
+    it('should render a HeaderLink for wedding party', () => {
+        expect(renderedWeddingPartyLink.type).toBe(HeaderLink);
+        expect(renderedWeddingPartyLink.props.selected).toBe(expectedProps.location.pathname);
+        expect(renderedWeddingPartyLink.props.route).toBe('/wedding-party');
+        expect(renderedWeddingPartyLink.props.text).toBe('WEDDING PARTY');
+    });
+
+    it('should render a HeaderLink for pictures', () => {
+        expect(renderedPicturesLink.type).toBe(HeaderLink);
+        expect(renderedPicturesLink.props.selected).toBe(expectedProps.location.pathname);
+        expect(renderedPicturesLink.props.route).toBe('/pictures');
+        expect(renderedPicturesLink.props.text).toBe('PICTURES');
+    });
+
+    it('should render a HeaderLink for registry', () => {
+        expect(renderedRegistryLink.type).toBe(HeaderLink);
+        expect(renderedRegistryLink.props.selected).toBe(expectedProps.location.pathname);
+        expect(renderedRegistryLink.props.route).toBe('/registry');
+        expect(renderedRegistryLink.props.text).toBe('REGISTRY');
+    });
 });
--- test/components/Person.test.js
+++ test/components/Person.test.js
@@ -1,18 +1,104 @@
 import React from 'react';
 import Chance from 'chance';
 import ShallowRenderer from 'react-test-renderer/shallow';
-import BrideGroom from '../../src/components/BrideGroom';
+import Person from '../../src/components/Person';
 
 const chance = new Chance();
 
 describe('Person', () => {
+    let expectedProps,
+
+        renderedComponent,
+
+        renderedImageDiv,
+        renderedInfoDiv,
+
+        renderedImage,
+
+        renderedName,
+        renderedRelation,
+        renderedBio,
+
+        renderedBoldName;
+
+    const cacheChildren = () => {
+        [
+            renderedImageDiv,
+            renderedInfoDiv
+        ] = renderedComponent.props.children;
+
+        renderedImage = renderedImageDiv.props.children;
+
+        [
+            renderedName,
+            renderedRelation,
+            renderedBio
+        ] = renderedInfoDiv.props.children;
+
+        renderedBoldName = renderedName.props.children;
+    };
+
     const renderComponent = () => {
         const shallowRenderer = ShallowRenderer.createRenderer();
 
-        shallowRenderer.render(<BrideGroom {...expectedProps}/>);
+        shallowRenderer.render(<Person {...expectedProps}/>);
 
         renderedComponent = shallowRenderer.getRenderOutput();
 
         cacheChildren();
     };
+
+    beforeEach(() => {
+        expectedProps = {
+            info: {
+                name: chance.string(),
+                role: chance.string(),
+                relation: chance.string(),
+                bio: chance.string()
+            }
+        };
+
+        renderComponent();
+    });
+
+    it('should render a root div', () => {
+        expect(renderedComponent.type).toBe('div');
+        expect(renderedComponent.props.className).toBe('Person-wrapper row');
+    });
+
+    it('should render a div for the image', () => {
+        expect(renderedImageDiv.type).toBe('div');
+        expect(renderedImageDiv.props.className).toBe('Person-imageWrapper');
+    });
+
+    it('should render the image', () => {
+        expect(renderedImage.type).toBe('img');
+        expect(renderedImage.props.alt).toBe('');
+        expect(renderedImage.props.className).toBe('Person-image');
+        expect(renderedImage.props.src).toBe(expectedProps.info.image);
+    });
+
+    it('should render a div for the information', () => {
+        expect(renderedInfoDiv.type).toBe('div');
+        expect(renderedInfoDiv.props.className).toBe('Person-text column spaceEvenly');
+    });
+
+    it('should render the name and role wrapper', () => {
+        expect(renderedName.type).toBe('p');
+    });
+
+    it('should render the bold name and role', () => {
+        expect(renderedBoldName.type).toBe('b');
+        expect(renderedBoldName.props.children).toBe(`${expectedProps.info.name} - ${expectedProps.info.role}`);
+    });
+
+    it('should render the relation', () => {
+        expect(renderedRelation.type).toBe('p');
+        expect(renderedRelation.props.children).toBe(`${expectedProps.info.relation}`);
+    });
+
+    it('should render the bio', () => {
+        expect(renderedBio.type).toBe('p');
+        expect(renderedBio.props.children).toBe(`${expectedProps.info.bio}`);
+    });
 });
